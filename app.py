1eb9cfd4b7dc0dd1ccb976f0145cc6f95e7f5527
Zoey Lu <luchin601@gmail.com>
2023-09-05 02:07:24 +0800

    add response header for allow origin

--- app.py
+++ app.py
@@ -19,40 +19,47 @@ def thankyou():
 	return render_template("thankyou.html")
 
 #API
+error_message = {
+			"error": True,
+			"message": "請按照情境提供對應的錯誤訊息"}
+header = {
+			"Access-Control-Allow-Origin" : "*"
+		}
+
 @app.route("/api/attractions")
 def get_attractions_by_page_api():
 	page = request.args.get("page")
 	keyword = request.args.get("keyword")
 	try:
 		data = get_attractions_by_page(page,keyword)
-		return jsonify(data),200
+		res = make_response(jsonify(data),200,header)
+		return res
 	except Exception as e:
 		print(e)
-		return jsonify({
-			"error": True,
-			"message": "請按照情境提供對應的錯誤訊息"}),500
+		res= make_response(jsonify(error_message),500,header)
+		return res
 
 @app.route("/api/attraction/<attractionId>")
 def get_attraction_by_id_api(attractionId):
 	try:
 		data = get_attraction_by_id(attractionId)
 		status_code = 400 if data.get("error") else 200
-		return jsonify(data),status_code
+		res = make_response(jsonify(data),status_code,status_code)
+		return res
 	except Exception as e:
 		print(e)
-		return jsonify({
-			"error": True,
-			"message": "請按照情境提供對應的錯誤訊息"}),500
+		res= make_response(jsonify(error_message),500,header)
+		return res
 
 @app.route("/api/mrts")
 def get_mrts_api():
 	try:
 		data = get_mrts()
-		return jsonify(data),200
+		res = make_response(jsonify(data),200,header)
+		return res
 	except Exception as e:
 		print(e)
-		return jsonify({
-			"error": True,
-			"message": "請按照情境提供對應的錯誤訊息"}),500
+		res= make_response(jsonify(error_message),500,header)
+		return res
 
 app.run(host="0.0.0.0", port=3000)
