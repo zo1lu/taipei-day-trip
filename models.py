f342551a137d079900d4fe827a0e58a1534be350
Zoey Lu <luchin601@gmail.com>
2023-09-06 17:46:32 +0800

    1.Changing method of getting next page value by select next page first data

--- models.py
+++ models.py
@@ -8,57 +8,49 @@ DB_CONFIG = {
     'host': '127.0.0.1',
     'database':'taipeitripweb'
 }
-
-def get_attractions_by_page(page,keyword):
+def get_attactions_with_keyword(keyword, start_index, data_num):
     con = mysql.connect(**DB_CONFIG)
     cursor_data = con.cursor(dictionary=True)
-    i = int(page)*12
-    data_per_page = 12
+    query_data = "SELECT site.id, site.name, site.category, site.description, site.address, site.transport, mrts.mrt_name as mrt, site.latitude as lat, site.longitude as lng, GROUP_CONCAT(images.image_url) as images FROM attractions as site LEFT JOIN mrts ON site.mrt_id = mrts.id JOIN images ON site.id = images.attraction_id WHERE mrts.mrt_name like %(precise_key)s OR site.name like %(fuzzy_key)s GROUP BY site.id ORDER BY site.id LIMIT %(data_num)s OFFSET %(start_index)s"
+    parameter = {
+        "precise_key":keyword,
+        "fuzzy_key":"%{}%".format(keyword),
+        "data_num":data_num,
+        "start_index":start_index
+    }
+    cursor_data.execute(query_data, parameter)
+    data = cursor_data.fetchall()
+    return data
+
+def get_attractions_without_keyword(start_index, data_num):
+    con = mysql.connect(**DB_CONFIG)
+    cursor_data = con.cursor(dictionary=True)
+    query_data="SELECT site.id, site.name, site.category, site.description, site.address, site.transport, mrts.mrt_name as mrt, site.latitude as lat, site.longitude as lng, GROUP_CONCAT(images.image_url) as images FROM attractions as site LEFT JOIN mrts ON site.mrt_id = mrts.id JOIN images ON site.id = images.attraction_id GROUP BY site.id ORDER BY site.id LIMIT %(data_num)s OFFSET %(start_index)s"
+    parameter = {
+        "data_num":data_num,
+        "start_index":start_index
+    }
+    cursor_data.execute(query_data, parameter)
+    data = cursor_data.fetchall()
+    return data
+
+def get_attractions_by_page(page,keyword):
 
     if keyword!=None:
-        query_data = "SELECT site.id, site.name, site.category, site.description, site.address, site.transport, mrts.mrt_name as mrt, site.latitude as lat, site.longitude as lng, GROUP_CONCAT(images.image_url) as images FROM attractions as site LEFT JOIN mrts ON site.mrt_id = mrts.id JOIN images ON site.id = images.attraction_id WHERE mrts.mrt_name like %(precise_key)s OR site.name like %(fuzzy_key)s GROUP BY site.id ORDER BY site.id LIMIT %(data_per_page)s OFFSET %(start_index)s"
-        parameter = {
-            "precise_key":keyword,
-            "fuzzy_key":"%{}%".format(keyword),
-            "data_per_page":data_per_page,
-            "start_index":i
-        }
-        cursor_data.execute(query_data,parameter)
+        data = get_attactions_with_keyword(keyword,int(page)*12,12)
+        #check next page data existance
+        next_page_data = get_attactions_with_keyword(keyword,(int(page)+1)*12,1)
     else:
-        query_data="SELECT site.id, site.name, site.category, site.description, site.address, site.transport, mrts.mrt_name as mrt, site.latitude as lat, site.longitude as lng, GROUP_CONCAT(images.image_url) as images FROM attractions as site LEFT JOIN mrts ON site.mrt_id = mrts.id JOIN images ON site.id = images.attraction_id GROUP BY site.id ORDER BY site.id LIMIT %(data_per_page)s OFFSET %(start_index)s"
-        parameter = {
-            "data_per_page":data_per_page,
-            "start_index":i
-        }
-        cursor_data.execute(query_data,parameter)
+        data = get_attractions_without_keyword(int(page)*12,12)
+        #check next page data existance
+        next_page_data = get_attactions_with_keyword((int(page)+1)*12,1)
 
-    data = cursor_data.fetchall()
     result_data = list(map(lambda site:{**site,"images":site["images"].split(",")},data))
 
-    data_size = get_attractions_count(keyword)
-
     return {    
-            "nextPage":int(page)+1 if data_size>(int(page)+1)*12 else None,
+            "nextPage":int(page)+1 if len(next_page_data)==1 else None,
             "data":result_data
         }
-
-def get_attractions_count(keyword):
-    con = mysql.connect(**DB_CONFIG)
-    cursor_data_count = con.cursor(dictionary=True)
-    if keyword!=None:
-        query_data_count = "SELECT COUNT(*) as size FROM attractions as site LEFT JOIN mrts ON site.mrt_id = mrts.id WHERE mrts.mrt_name like %(precise_key)s OR site.name like %(fuzzy_key)s"
-        parameter = {
-            "precise_key":keyword,
-            "fuzzy_key":"%{}%".format(keyword)
-        }
-        cursor_data_count.execute(query_data_count,parameter)
-    else:
-        query_data_count = "SELECT COUNT(*) as size FROM attractions"
-        cursor_data_count.execute(query_data_count)
-
-    count = cursor_data_count.fetchone()
-    data_size = count["size"]
-    return data_size
 
 def get_attraction_by_id(id):
     try:
@@ -87,5 +79,3 @@ def get_mrts():
     result_data = list(map(lambda mrt:mrt["mrt_name"],data))
     return {"data":result_data}
 
-# get_attraction_by_id(1)
-# get_mrts();
