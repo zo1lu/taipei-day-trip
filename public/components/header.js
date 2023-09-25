a67d9fce3189838a753bf028cb23bb526f98914d
Zoey Lu <luchin601@gmail.com>
2023-09-26 00:13:00 +0800

    Create booking page

--- public/components/header.js
+++ public/components/header.js
@@ -7,7 +7,7 @@ class Header extends HTMLElement {
         <div class="head">
             <a class="head__title" href="/">台北一日遊</a>
             <div class="navbar">
-                <a class="navbar__link">預定行程</a>
+                <a class="navbar__link" href="/booking" onclick="checkLogin()">預定行程</a>
                 <a class="navbar__link" id="login_btn" onclick="switchPopupDisplay(false)">登入/註冊</a>
                 <a class="navbar__link link__logout" id="logout_btn" onclick="logout()">&nbsp&nbsp登出&nbsp&nbsp</a>
             </div>
